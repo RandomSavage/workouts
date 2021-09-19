bf84954e0db9ff965cf92ea8e085bc1ccbdd0578
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-09-19 14:16:46 -0400

    divide by 3 with user given range

--- dualSport/js/div3.js
+++ dualSport/js/div3.js
@@ -1,3 +1,45 @@
+// using prompts and alerts to mimic java scanner 
+// nodejs would be closer but this is just js for now
+
+
+let div3range = (start, end) => {
+  let counter = start;
+    while(counter <= end) {
+      if(counter % 3 === 0) {
+        console.log(counter);
+      }
+      counter += 1;
+    }
+}
+
+while(true) {
+  let exitCode = prompt("To exit: type (exit) / enter to (continue)", "Do you want to continue or exit?");
+
+  if(exitCode === "exit") {
+    break;
+  } else {
+    let start = parseInt(prompt("start #"));
+    let end = parseInt(prompt("end #"));
+    div3range(start, end);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* import java.util.Scanner;
 
 import javax.sound.sampled.SourceDataLine;
